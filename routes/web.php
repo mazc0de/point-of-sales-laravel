aeef460833e5cf685e9ba688b8b0bcac3396611f
mazc0de <dandroid36@gmail.com>
2021-02-11 19:06:56 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
+Route::middleware('auth')->group(function(){
+    Route::prefix('users')->group(function(){
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::get('show/{user:id}', [UserController::class, 'show'])->name('users.show');
+        Route::get('create', [UserController::class, 'create'])->name('users.create');
+        Route::post('create', [UserController::class, 'store']);
+        Route::get('edit/{user:id}', [UserController::class, 'edit'])->name('users.edit');
+        Route::put('edit/{user:id}', [UserController::class, 'update']);
+        Route::get('delete/{user:id}', [UserController::class, 'destroy'])->name('users.delete');
+    });
+    Route::resource('roles', RoleController::class);
+    Route::resource('products', ProductController::class);
+});
