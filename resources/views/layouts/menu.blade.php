1bf0566ed997ece6a4f519acb519b22e0733aea7
mazc0de <dandroid36@gmail.com>
2021-02-11 19:15:08 +0700

    update menu sidebar

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,10 @@
 <li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
-    <a class="nav-link" href="/">
+    <a class="nav-link" href="/dashboard">
         <i class=" fas fa-building"></i><span>Dashboard</span>
+    </a>
+</li>
+<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
+    <a class="nav-link" href="{{route('users.index')}}">
+        <i class=" fas fa-building"></i><span>Users</span>
     </a>
 </li>
