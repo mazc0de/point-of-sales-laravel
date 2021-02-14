f64265f729ea631f3b69838a9382ec2017effc8f
mazc0de <dandroid36@gmail.com>
2021-02-14 21:35:54 +0700

    update sidebar menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,10 +1,20 @@
-<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
+<li class="side-menus {{ Route::currentRouteNamed('home') ? 'active' : '' }}">
     <a class="nav-link" href="/dashboard">
         <i class=" fas fa-building"></i><span>Dashboard</span>
     </a>
 </li>
-<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{route('users.index')}}">
-        <i class=" fas fa-building"></i><span>Users</span>
-    </a>
+
+<li class="nav-item dropdown {{ Route::currentRouteNamed('users.*') || Route::currentRouteNamed('roles.*')  ? 'active' : ''}}">
+    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>User Management</span></a>
+    <ul class="dropdown-menu" style="display: none;">
+        <li class="{{ Route::currentRouteNamed('users.*') ? 'active' : ''}}">
+            <a class="nav-link " href="{{ route('users.index')}}">Users</a>
+        </li>
+        @role('super-admin')
+            <li class="{{ Route::currentRouteNamed('roles.*') ? 'active' : ''}}">
+                <a class="nav-link " href="{{ route('roles.index')}}">Roles</a>
+            </li>
+        @endrole
+    </ul>
 </li>
+
